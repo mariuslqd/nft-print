0f549f672e3bc477d15e7964d5e01df9eed01f34
mariuslqd <119520955+mariuslqd@users.noreply.github.com>
2022-12-27 18:04:09 +0100

    test

--- app/wallet.tsx
+++ app/wallet.tsx
@@ -21,7 +21,12 @@ export default function Wallet() {
         <div>
             <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => disconnect()}>{address}
+                onClick={() => {
+                  disconnect();
+                  setNfts([]);
+                }}
+              >
+                {address}
             </button>
         </div>
       )
