3902acb4ec6ccf414e0115506725715082ddf6fd
mariuslqd <119520955+mariuslqd@users.noreply.github.com>
2022-12-27 17:32:03 +0100

    test

--- app/wallet.tsx
+++ app/wallet.tsx
@@ -10,7 +10,20 @@ export default function Wallet() {
     const router = useRouter();
     const [nfts, setNfts] = useState<Nft[]>([]);
    
-
+    if (isConnected) {
+      return (
+        <div>
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() =>   {          disconnect();
+                  setNfts([]);
+                }}
+              >
+                {address}
+            </button>
+        </div>
+      )
+    }
 
     if (isLoading) {
       router.refresh();
