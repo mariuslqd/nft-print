dec5f7ddeede7dedee6e99663c8f5afe129fa37f
mariuslqd <119520955+mariuslqd@users.noreply.github.com>
2022-12-27 17:51:00 +0100

    test

--- app/wallet.tsx
+++ app/wallet.tsx
@@ -15,20 +15,17 @@ export default function Wallet() {
         <div>
             <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() =>   {      setNfts([])  ;  disconnect();
-                  
-                }}
-              >
-                {address}
+                onClick={() => disconnect()}>{address}
             </button>
         </div>
       )
     }
 
+    useEffect(() => {
     if (isLoading) {
       router.refresh();
     }
-   
+    }, [isLoading]);
     return (
       <div>
         {connectors.map((connector) => (
