bcf64931fcc4ff0d45932c7deadd5a62b76addf7
mariuslqd <119520955+mariuslqd@users.noreply.github.com>
2022-12-27 15:31:52 +0100

    test

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -14,8 +14,8 @@ export default function Success() {
                 <h1 className="mb-4 text-4xl font-bold text-center text-gray-700">
                   Success!
                 </h1 >
-                <div className="mb-4 flex text-center mx-auto">
-                 <Image src="/giphy.gif" alt="Success gif" width={400} height={400} />
+                <div className="mb-4 block text-center mx-auto">
+                 <Image src="/giphy.gif" alt="Success gif" width={300} height={300} />
                  </div>
                  <h2 className="mb-4 text-1xl font-bold text-center text-gray-700">
                   Our Kitties are getting your order ready!
