7fa5f7565372ea3a87d6778f88feb9e4207615f0
mariuslqd <119520955+mariuslqd@users.noreply.github.com>
2022-12-27 17:15:04 +0100

    test

--- app/print/page.tsx
+++ app/print/page.tsx
@@ -64,6 +64,14 @@ export default function Print() {
                     className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
                     placeholder="Address" type="Address" />
                 </div>
+                <div>
+                  <label className="block text-sm">
+                    QR-Code Verification Name
+                  </label>
+                  <input type="text"
+                    className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
+                    placeholder="QR-Name" />
+                </div>
                 <Link href={{pathname: '/success',}}>
                 <button
                   className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -9,23 +9,23 @@ export default function Success() {
 
   return (
     <div className="flex items-center min-h-screen">
-    <div className="flex-1 h-full max-w-4xl mx-auto bg-white border rounded-md border-blue-400">
+    <div className="flex-1 h-full max-w-4xl mx-auto bg-white ">
       <div className="flex flex-col md:flex-row">
         <div className="h-100 md:h-auto md:w-1/2">
                 <Image src="/giphy.gif" alt="Success gif" width={600} height={600}  />
         </div>
-        <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+        <div className="flex items-center justify-center p-6 sm:p-12 md:w-1/2 border rounded-md border-blue-400">
               <div className="w-full">
               <h1 className="mb-4 text-4xl font-bold text-center text-gray-700">
                   Success!
                 </h1 >
                  <h2 className="mb-4 text-2xl font-bold text-center text-gray-700">
-                  What happens next:
+                  Next we will:
                  </h2>
-                       <h2 className="mb-4 text-xl font-bold text-left text-gray-700">
+                       <h2 className="mb-4 text-base font-bold text-left text-gray-700">
                         <ul>
-                          <li>1. We will 4x upscale your NFT via AI</li>
-                          <li>2. Create a QR Code, that identifies you as the owner </li>
+                          <li>1. 4x upscale your NFT via AI</li>
+                          <li>2. Create a QR Code, that identifies you as the owner</li>
                           <li>3. Design the Mockup and send it to you via email with the Payment instructions</li>
                           <li>4. After you approved the design and paid, we will send out your order</li>
                           <li>5. You will get the tracking details for your shipment send via Email</li>
