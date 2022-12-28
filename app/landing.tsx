bddf875ca2a8afd7df424c4e19f2f689d424b809
mariuslqd <119520955+mariuslqd@users.noreply.github.com>
2022-12-29 00:45:56 +0100

    test

--- app/landing.tsx
+++ app/landing.tsx
@@ -15,9 +15,21 @@ return (
               <div className="w-full">
               <h1 className="mb-4 text-5xl font-bold text-center text-gray-700">
                   Create your own
-                  <br>non-fungible</br>
+                  <br/>non-fungible
                   NFT prints
-                </h1>
+                </h1 >
+                 <h2 className="mb-4 text-lg font-bold text-center text-gray-700">
+                  Next we will:
+                 </h2>
+                       <h2 className="mb-4 text-lg font-bold text-left text-gray-700">
+                        <ul>
+                          <li>1. 4x upscale your NFT via AI</li>
+                          <li>2. Create a QR Code, that identifies you as the owner</li>
+                          <li>3. Design the Mockup and send it to you via email with the Payment instructions</li>
+                          <li>4. After you approved the design and paid, we will send out your order</li>
+                          <li>5. You will get the tracking details for your shipment send via Email</li>
+                         </ul>
+                      </h2>
               </div>
             </div>
           </div>
