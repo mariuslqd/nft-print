80f629f31c6a9f24e3fd299b8e75a89e1ab62a0a
mariuslqd <119520955+mariuslqd@users.noreply.github.com>
2022-12-29 00:28:19 +0100

    test

--- app/landing.tsx
+++ app/landing.tsx
@@ -11,7 +11,35 @@ export default function Landing() {
 
 return (
     <div className="bg-gradient bg-fade">
-<h1 className="text-white">Please connect a wallet to view your NFTs.</h1>
+   <div className="flex items-center min-h-screen">
+    <div className="flex-1 h-full max-w-4xl mx-auto ">
+      {/* <div className="flex flex-col md:flex-row">
+                <Image src="/giphy.gif" alt="Success gif" width={600} height={600}  />
+        </div> */}
+        <div className="flex items-center justify-center p-4 sm:p-6 md:w-1/2 border rounded-r-md border-blue-400">
+              <div className="w-full">
+              <h1 className="mb-4 text-5xl font-bold text-center text-gray-700">
+                  Create your own
+                  <br>non-fungible</br>
+                  NFT prints
+                </h1 >
+                 <h2 className="mb-4 text-lg font-bold text-center text-gray-700">
+                  Next we will:
+                 </h2>
+                       <h2 className="mb-4 text-lg font-bold text-left text-gray-700">
+                        <ul>
+                          <li>1. 4x upscale your NFT via AI</li>
+                          <li>2. Create a QR Code, that identifies you as the owner</li>
+                          <li>3. Design the Mockup and send it to you via email with the Payment instructions</li>
+                          <li>4. After you approved the design and paid, we will send out your order</li>
+                          <li>5. You will get the tracking details for your shipment send via Email</li>
+                         </ul>
+                      </h2>
+              </div>
+            </div>
+          </div>
+          </div>
+          </div>
 </div>
 )
 }
