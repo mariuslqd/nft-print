88d43cc3497085417464198331613038dbdb8e7a
mariuslqd <119520955+mariuslqd@users.noreply.github.com>
2022-12-28 22:55:53 +0100

    test

--- app/header.tsx
+++ app/header.tsx
@@ -8,15 +8,13 @@ import WagmiProvider from "../providers/wagmi";
 export default function Header() {
   return (
     <nav className="flex items-center justify-between flex-wrap p-6">
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div>
-          <Link href="/" className="mb-4 text-4xl font-extrabold tracking-tight leading-none dark:text-white">NFT Pod</Link>
+        <div className="logo">
+          <Link href="/" className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-white">NFT Pod</Link>
         </div>
         <div className="flex-grow">
         </div>
         <div>
         </div>
-      </div>
     </nav>
   )
 }
