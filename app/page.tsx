6b80a33b5eb0d6e255dfc66053914de89c52979a
mariuslqd <119520955+mariuslqd@users.noreply.github.com>
2022-12-28 22:17:15 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,23 @@
 import { Inter } from '@next/font/google'
 import NFTs from './nfts';
 
+import Wallet from './wallet';
+import ClientOnly from './clientOnly';
+import WagmiProvider from "../providers/wagmi";
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
       <NFTs />
+      <div className="floating-nft-wallet">
+        <ClientOnly>
+          <WagmiProvider>
+              <Wallet />
+          </WagmiProvider>
+        </ClientOnly>
     </div>
+      </div>
   )
 }
