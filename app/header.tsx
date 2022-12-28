aa85fece6d2816bc8424c2fd78fafb78e6d24ee0
mariuslqd <119520955+mariuslqd@users.noreply.github.com>
2022-12-28 22:34:01 +0100

    test

--- app/header.tsx
+++ app/header.tsx
@@ -9,10 +9,12 @@ export default function Header() {
   return (
     <nav className="flex items-center justify-between flex-wrap p-6">
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div>
+          <Link href="/" className="mb-4 text-4xl font-extrabold tracking-tight leading-none dark:text-black">NFT Pod</Link>
+        </div>
         <div className="flex-grow">
         </div>
         <div>
-
         </div>
       </div>
     </nav>
