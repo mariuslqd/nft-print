db3c69f6db581393a1148341c8878ca1d0db8c41
mariuslqd <119520955+mariuslqd@users.noreply.github.com>
2022-12-28 17:51:16 +0100

    test

--- app/wallet.tsx
+++ app/wallet.tsx
@@ -32,7 +32,7 @@ export default function Wallet() {
       <div>
         {connectors.map((connector) => (
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className=  "text-white font-bold py-2 px-4 rounded gradient-button animate-gradient duration-600 ease-in-out"
             disabled={!connector.ready}
             key={connector.id}
             onClick={() => connect({ connector })}
